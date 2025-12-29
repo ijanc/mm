e420b535941958afa6bca792b283a524e4041f0c
murilo ijanc <murilo@ijanc.org>
2025-12-28 21:27:01 -0300

    More subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(name = "mm")]
+#[command(version = "0.1.0")]
+#[command(about = "my manager", long_about = None)]
 struct Cli {
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    // /// Turn debugging information on
+    // #[arg(short, long, action = clap::ArgAction::Count)]
+    // debug: u8,
 
     #[command(subcommand)]
     command: Commands,
@@ -13,31 +15,83 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
+    /// People (Team & Culture)
+    ///
+    /// Hiring, coaching, mentoring, career growth, performance management,
+    /// psychological safety, fostering an inclusive culture, handling
+    /// difficult conversations.
+    People {
+        #[command(subcommand)]
+        cmd: PeopleCommands,
+    },
+
+    /// Product (Vision & Strategy)
+    ///
+    /// Understanding business goals, defining team vision, connecting work to
+    /// company strategy, aligning with stakeholders, creating roadmaps.
+    Product {
+        #[command(subcommand)]
+        cmd: ProductCommands,
+    },
+    
+    /// Process (Execution & Delivery)
+    /// 
+    /// Planning, project management, feedback loops, metrics, operational
+    /// excellence, risk management, efficient workflows, building scalable
+    /// systems.
+    Process {
+        #[command(subcommand)]
+        cmd: ProcessCommands,
+    },
+
+    /// Technology (Technical Leadership)
+    /// 
+    /// Ensuring quality, managing tech debt, driving innovation, architectural
+    /// decisions, maintaining system health, understanding the tech landscape
+    Technology {
+        #[command(subcommand)]
+        cmd: TechnologyCommands,
     },
 }
+
+#[derive(Subcommand)]
+enum PeopleCommands {
+    Team,
+    Culture
+}
+
+#[derive(Subcommand)]
+enum ProductCommands {
+    Vision,
+    Strategy
+}
+
+#[derive(Subcommand)]
+enum ProcessCommands { }
+
+#[derive(Subcommand)]
+enum TechnologyCommands { }
 
 fn main() {
     let cli = Cli::parse();
 
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
+    // match cli.debug {
+    //     0 => println!("Debug mode is off"),
+    //     1 => println!("Debug mode is kind of on"),
+    //     2 => println!("Debug mode is on"),
+    //     _ => println!("Don't be crazy"),
+    // }
 
     match &cli.command {
-        Commands::Test { list } => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
+        Commands::People { cmd } => match cmd {
+            PeopleCommands::Team => println!(""),
+            PeopleCommands::Culture=> println!(""),
         }
+        Commands::Product { cmd } => match cmd {
+            ProductCommands::Vision => println!(""),
+            ProductCommands::Strategy => println!(""),
+        }
+        Commands::Process { cmd: _ } => todo!(),
+        Commands::Technology { cmd: _ } => todo!(),
     }
 }
